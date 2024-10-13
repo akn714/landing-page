0d2a506a16e2682fed87136883bee6711f945700
akn714 <adarshkumar20012704@gmail.com>
2024-10-14 02:47:47 +0530

    completed all tasks

--- script.js
+++ script.js
@@ -3,40 +3,40 @@ document.addEventListener('DOMContentLoaded', function() {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');
-        burger.classList.toggle('active');
+    // burger.addEventListener('click', () => {
+    //     // Toggle Nav
+    //     nav.classList.toggle('nav-active');
+    //     burger.classList.toggle('active');
 
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
+    //     // Animate Links
+    //     navLinks.forEach((link, index) => {
+    //         if (link.style.animation) {
+    //             link.style.animation = '';
+    //         } else {
+    //             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    //         }
+    //     });
 
-        // Burger Animation
-        burger.classList.toggle('toggle');
-    });
+    //     // Burger Animation
+    //     burger.classList.toggle('toggle');
+    // });
     
     // Smooth scrolling for navigation links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            nav.classList.toggle('nav-active');
-            burger.classList.toggle('active');
-            burger.classList.toggle('toggle');
-            navLinks.forEach((link, index) => {
-                link.style.animation = '';
-            });
+    // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    //     anchor.addEventListener('click', function (e) {
+    //         e.preventDefault();
+    //         nav.classList.toggle('nav-active');
+    //         burger.classList.toggle('active');
+    //         burger.classList.toggle('toggle');
+    //         navLinks.forEach((link, index) => {
+    //             link.style.animation = '';
+    //         });
 
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
+    //         document.querySelector(this.getAttribute('href')).scrollIntoView({
+    //             behavior: 'smooth'
+    //         });
+    //     });
+    // });
 });
 
 window.addEventListener('scroll', function() {
